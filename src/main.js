17483f4b8a419cf51621e551c4c81d9c6de382ff
superxmanyang <460701143@qq.com>
2019-08-22 21:03:27 +0800

    路由的创建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入自定义样式
+import '@/styles/index.less'
+// 引入Element
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router/router.js'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
