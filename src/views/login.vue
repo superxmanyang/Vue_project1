484e4ad45271ed5afd1ed4f9f7c9964987e27987
superxmanyang <460701143@qq.com>
2019-08-23 15:33:18 +0800

    '05-登陆组件的制作'

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,37 @@
 <template>
-    <div class="login">登陆</div>
+    <div class="login">
+<el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="100px" class="demo-ruleForm">
+  <el-form-item label="活动名称" prop="username">
+    <el-input v-model="loginForm.username"></el-input>
+    <!-- READme接口文档把name改成username -->
+  </el-form-item>
+
+  <el-form-item>
+    <el-button type="primary" >立即创建</el-button>
+    <el-button >重置</el-button>
+  </el-form-item>
+</el-form>
+</div>
 </template>
 
 <script>
 export default {
+  data () {
+    //   <!-- READme接口文档把name改成username -->
 
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  rules: {
+    username: [
+      { required: true, message: '请输入活动名称', trigger: 'blur' },
+      { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+    ]
+  }
 }
 </script>
 
