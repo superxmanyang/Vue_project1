09d614a16ec3c98e2d4951f9b556839fc241d5f4
superxmanyang <460701143@qq.com>
2019-08-23 15:49:59 +0800

    添加验证规则''

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,17 +1,27 @@
 <template>
-    <div class="login">
-<el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="100px" class="demo-ruleForm">
-  <el-form-item label="活动名称" prop="username">
-    <el-input v-model="loginForm.username"></el-input>
-    <!-- READme接口文档把name改成username -->
-  </el-form-item>
+  <div class="login">
+    <el-form
+      :model="loginForm"
+      :rules="rules"
+      ref="loginForm"
 
-  <el-form-item>
-    <el-button type="primary" >立即创建</el-button>
-    <el-button >重置</el-button>
-  </el-form-item>
-</el-form>
-</div>
+      class="demo-ruleForm"
+    >
+      <el-form-item  prop="username">
+        <el-input v-model="loginForm.username" placeholder='请输入用户名'></el-input>
+        <!-- READme接口文档把name改成username -->
+      </el-form-item>
+      <el-form-item  prop="password">
+        <el-input v-model="loginForm.password" placeholder='请输入密码'></el-input>
+        <!-- READme接口文档把name改成username -->
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary">立即创建</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -23,18 +33,22 @@ export default {
       loginForm: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          // required:必填，不能为空
+          // trigger：什么时候触发这个验证规则
+          // message：如果不符合验证规则时的提示信息
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
       }
     }
-  },
-  rules: {
-    username: [
-      { required: true, message: '请输入活动名称', trigger: 'blur' },
-      { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-    ]
   }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
