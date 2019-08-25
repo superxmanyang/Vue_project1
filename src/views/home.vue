5e4b03c8471605a8c34bb001dc6163d5d866eb32
superxmanyang <460701143@qq.com>
2019-08-25 15:46:25 +0800

    '完成左侧导航的添加'

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,9 +3,10 @@
     <el-container>
       <el-aside width="200px">
         <el-menu
-          default-active="2"
+        :router="true"
+          :default-active="'2-1'"
           class="el-menu-vertical-demo"
-
+          :unique-opened="true"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
@@ -15,7 +16,7 @@
               <i class="el-icon-location"></i>
               <span>用户管理</span>
             </template>
-           <el-menu-item index="1-3">
+           <el-menu-item index="1-1">
                <template slot="title">
               <i class="el-icon-menu"></i>
               <span>用户列表</span>
@@ -23,18 +24,18 @@
            </el-menu-item>
 
           </el-submenu>
-             <el-submenu index="1">
+             <el-submenu index="2">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span>权限管理</span>
             </template>
-           <el-menu-item index="1-3">
+           <el-menu-item index="/login">
                <template slot="title">
               <i class="el-icon-menu"></i>
               <span>权限列表</span>
             </template>
            </el-menu-item>
-            <el-menu-item index="1-3">
+            <el-menu-item index="2-2">
                <template slot="title">
               <i class="el-icon-menu"></i>
               <span>角色列表</span>
